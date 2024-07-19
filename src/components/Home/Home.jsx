3fd09a61de67f80947623dd940d693df8bca50a6
tj <rossettitj@gmail.com>
2024-07-18 20:24:55 -0400

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import {Link, Outlet} from "react-router-dom"
-import ronaldo from '/images/ronaldo2.webp'
+import ronaldo from 'public/images/ronaldo2.webp'
 import messi from '/images/messi.avif'
 
 
