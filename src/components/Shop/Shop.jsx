7eff0d288ff2390021b56d129bdaef49b7322858
tj <rossettitj@gmail.com>
2024-07-18 20:06:58 -0400

    dsf
    sdf

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,41 +4,12 @@ import axios from "axios"
 
 
 export default function Shop() {
-  const [category, setCategory] = useState(test);
-  const handleBtns = (e) => {
-  let word=e.target.value;
-   
-  if(word === "All"){
-    setCategory(db)
-  }
-  else if(word === "Cars") {
-    const filtered = db.filter(item=>item.kind === "Cars");
-    setCategory(filtered)
-  }
-  else if(word === "Views") {
-    const filtered = db.filter(item=>item.kind === "Views");
-    setCategory(filtered)
-  }
-}
+
 
   return (
 
     <div>
-          <div>
-            <button value="All" onClick={handleBtns}>All</button>
-            <button value="Cars" onClick={handleBtns}>Cars</button>
-            <button value="Views" onClick={handleBtns}>Views</button>
-          </div>
-
-
-      {category.map((item) => (
-        <div className='card'>
-          <p>{item.product_name}</p>
-          <p>{item.price}</p>
-          <p>{item.description}</p>
-          <p>{item.catergory}</p>
-        </div>
-      ))}
+      <p>hi</p>
     </div>
   )
 }
